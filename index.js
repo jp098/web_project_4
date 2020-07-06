55acbe08f39ad0dc3ba3f2d43587a705cfe8fff8
Jair Palacios <jairpalacios386@gmail.com>
2020-07-05 22:59:23 -0400

    fixed html, css, and javascript files

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-button');
 const popup = document.querySelector('.popup');
 const form = document.querySelector('.form');
+const inputName = document.querySelector('.form__input_type_name');
+const inputAboutme = document.querySelector('.form__input_type_about-me');
+const profileName = document.querySelector('.profile__name');
+const profileAboutme = document.querySelector('.profile__about-me');
+
 
 
 function togglePopup() {
@@ -19,12 +24,6 @@ closeButton.addEventListener('click', () => {
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-
-    const inputName = document.querySelector('.form__input_type_name');
-    const inputAboutme = document.querySelector('.form__input_type_about-me');
-
-    const profileName = document.querySelector('.profile__name');
-    const profileAboutme = document.querySelector('.profile__about-me');
 
     profileName.textContent = inputName.value;
     profileAboutme.textContent = inputAboutme.value;
